6854f45f63309a48a512df5fb948ae808aa370ff
masadmunawar <masad.techtimize@gmail.com>
2023-02-06 19:57:35 +0500

    Calculate Diagnosis Result working

--- src/routers/daignonsisResult.js
+++ src/routers/daignonsisResult.js
@@ -1,15 +1,62 @@
 const express = require("express");
 const router = express.Router();
 const DiagnosisResult = require("../models/diagnosisResult");
+const Probability = require("../models/probability");
+const AssignResult = require("../models/assignResult");
+const painPossibleDiag = require("../models/painPossibleDiagnostics");
+const { ObjectId } = require('mongodb');
+
+const getProbabilityByPainBehaviorId = (request) => {
+  const prob = Probability.find({ painBehaviorId: request.painBehaviorId });
+  return prob;
+}
+const getAssignResultByPainBehaviorId = (request) => {
+  const results = AssignResult.find({
+    painBehaviorId: request.painBehaviorId
+  },{
+    __v : 0,
+    painBehaviorId : 0
+  }).populate("painBehaviorQuestionId");
+  return results;
+}
+const possibleDiagnosis = (request) => {
+  const getPainPossibleDiag = painPossibleDiag.find({
+    painBehaviorId: request.painBehaviorId,
+    isPossibleDiag: true
+  }, {
+    isPossibleDiag: 0,
+    painBehaviorId: 0
+  }).populate("diagnosticsId");
+  return getPainPossibleDiag;
+}
 
 
-// Get all diagnosis results
-router.get("/diagnosisResult", async (req, res) => {
+router.get("/calculateDiagnotics", async (req, res) => {
   try {
-    // Retrieve all diagnosis results from the database
-    const allDiagnosisResults = await DiagnosisResult.find();
-    // Respond with a status code of 200 and the retrieved data
-    res.status(200).send(allDiagnosisResults);
+    const getProbability = await getProbabilityByPainBehaviorId(req.body);
+    const probability = getProbability[0].probability;
+    console.log(probability);
+
+    const populateDiagnosis = await possibleDiagnosis(req.body);
+    const dignosis = populateDiagnosis.map((_diagnosis) => _diagnosis.diagnosticsId);
+
+    const assignResult = await getAssignResultByPainBehaviorId(req.body);
+
+    const result = assignResult.filter(element => element.painBehaviorQuestionId && element.DiagAnswer)
+  .map(element => {
+    if (element.painBehaviorQuestionId.questionId === new ObjectId(req.body.questionAnswer.questionId) &&
+      element.DiagAnswer === req.body.questionAnswer.isYes) {
+      return element;
+    }
+  });
+    console.log(result.length);
+    console.log(result);
+    console.log(new ObjectId(req.body.questionAnswer[0].questionId));
+    console.log(req.body.questionAnswer[0].isYes);
+    console.log(assignResult[0].painBehaviorQuestionId.questionId);
+    console.log(assignResult[0].Percentage);
+
+    res.status(200).send(assignResult);
   } catch (err) {
     // Respond with a status code of 404 and the error message if there was an issue retrieving the data
     res.status(404).send(err);
