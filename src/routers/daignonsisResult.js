688a3f83b9ae8c33db3523d42436e38e29bfd4bf
masadmunawar <masad.techtimize@gmail.com>
2023-02-07 20:40:57 +0500

    Creates the CalculateResult API

--- src/routers/daignonsisResult.js
+++ src/routers/daignonsisResult.js
@@ -4,19 +4,22 @@ const DiagnosisResult = require("../models/diagnosisResult");
 const Probability = require("../models/probability");
 const AssignResult = require("../models/assignResult");
 const painPossibleDiag = require("../models/painPossibleDiagnostics");
+const PainBehaviorQuestion = require("../models/painBehaviorQuestion");
 const { ObjectId } = require('mongodb');
 
 const getProbabilityByPainBehaviorId = (request) => {
   const prob = Probability.find({ painBehaviorId: request.painBehaviorId });
   return prob;
 }
-const getAssignResultByPainBehaviorId = (request) => {
+const getAssignResultByPainBehaviorId = (request, question, index) => {
   const results = AssignResult.find({
-    painBehaviorId: request.painBehaviorId
-  },{
-    __v : 0,
-    painBehaviorId : 0
-  }).populate("painBehaviorQuestionId");
+    painBehaviorId: request.painBehaviorId,
+    DiagAnswer: request.questionAnswer[index].isYes,
+    painBehaviorQuestionId: question[index]._id
+  }, {
+    Percentage: 1,
+    possibleDiagnosticId: 1
+  });
   return results;
 }
 const possibleDiagnosis = (request) => {
@@ -29,34 +32,43 @@ const possibleDiagnosis = (request) => {
   }).populate("diagnosticsId");
   return getPainPossibleDiag;
 }
-
+const getPainBehaviorQuestion = (request) => {
+  const result = PainBehaviorQuestion.find({ painBehaviorId: request.painBehaviorId }, { _id: 1 });
+  return result;
+}
 
 router.get("/calculateDiagnotics", async (req, res) => {
   try {
     const getProbability = await getProbabilityByPainBehaviorId(req.body);
     const probability = getProbability[0].probability;
-    console.log(probability);
 
     const populateDiagnosis = await possibleDiagnosis(req.body);
     const dignosis = populateDiagnosis.map((_diagnosis) => _diagnosis.diagnosticsId);
 
-    const assignResult = await getAssignResultByPainBehaviorId(req.body);
-
-    const result = assignResult.filter(element => element.painBehaviorQuestionId && element.DiagAnswer)
-  .map(element => {
-    if (element.painBehaviorQuestionId.questionId === new ObjectId(req.body.questionAnswer.questionId) &&
-      element.DiagAnswer === req.body.questionAnswer.isYes) {
-      return element;
+    let result = [];
+    const painBehaviorQuestion = await getPainBehaviorQuestion(req.body);
+    for (i = 0; i < painBehaviorQuestion.length; i++) {
+      let data = await getAssignResultByPainBehaviorId(req.body, painBehaviorQuestion, i);
+      result = result.concat(data);
     }
-  });
-    console.log(result.length);
-    console.log(result);
-    console.log(new ObjectId(req.body.questionAnswer[0].questionId));
-    console.log(req.body.questionAnswer[0].isYes);
-    console.log(assignResult[0].painBehaviorQuestionId.questionId);
-    console.log(assignResult[0].Percentage);
-
-    res.status(200).send(assignResult);
+    let resultPercentage = [];
+    let per = 0;
+    for(i=0; i<populateDiagnosis.length; i++){
+      for(j=0; j<result.length; j++){
+        if(JSON.stringify(result[j].possibleDiagnosticId) === JSON.stringify(populateDiagnosis[i]._id)){
+          per = per+result[j].Percentage;
+        }
+      }
+      let percentage = Math.round(per+probability);
+      per = 0;
+      let obj ={
+        possibleDiagnostic:populateDiagnosis[i].diagnosticsId.diagnosisName,
+        Percentage : percentage
+      };
+      resultPercentage.push(obj);
+    }
+    //console.log(result.length)
+    res.status(200).send(resultPercentage);
   } catch (err) {
     // Respond with a status code of 404 and the error message if there was an issue retrieving the data
     res.status(404).send(err);
